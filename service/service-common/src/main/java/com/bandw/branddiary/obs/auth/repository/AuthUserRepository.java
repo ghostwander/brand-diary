d181da61876e3d6f18c4fa39dfa33479d6c8850f
codengzihao <bandwdevelop@163.com>
2017-08-31 18:37:49 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/core.iml
    #	service/pom.xml
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/Application.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/auth/entity/AuthUser.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/auth/exception/AuthenticationException.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/auth/repository/AuthUserRepository.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/auth/service/AuthService.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/user/entity/User.java
    #	service/service-common/src/main/java/com/bandw/branddiary/obs/user/entity/UserBinding.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/Application.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/auth/model/AuthUser.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/auth/repository/AuthUserRepository.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/auth/service/AuthService.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/user/model/User.java
    #	service/service-common/src/main/java/com/bandw/branddiary/oss/user/model/UserBinding.java
    #	service/service-foundation/pom.xml
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/Application.java
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/auth/entity/AuthUser.java
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/auth/repository/AuthUserRepository.java
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/auth/service/AuthService.java
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/user/entity/User.java
    #	service/service-foundation/src/main/java/com/bandw/branddiary/obs/foundation/user/entity/UserBinding.java

--- service/service-common/src/main/java/com/bandw/branddiary/obs/auth/repository/AuthUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bandw.branddiary.obs.auth.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.bandw.branddiary.obs.auth.entity.AuthUser;
-
-public interface AuthUserRepository extends MongoRepository<AuthUser, Long> {
-
-}
--- service/service-common/src/main/java/com/bandw/branddiary/obs/auth/rpc/AuthRpc.java
+++
@@ -1,6 +0,0 @@
-package com.bandw.branddiary.obs.auth.rpc;
-
-import com.bandw.branddiary.core.rpc.BaseRpc;
-
-public class AuthRpc extends BaseRpc{
-}

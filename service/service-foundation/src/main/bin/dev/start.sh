14be53a88b789674f2cda2f409aa738feab74900
bandwdevelop <bandwdevelop@163.com>
2017-11-11 17:32:27 +0800

    modify some file for test program

--- service/service-foundation/src/main/bin/dev/start.sh
+++ service/service-foundation/src/main/bin/dev/start.sh
@@ -5,5 +5,5 @@ ARTIFACTID=${project.artifactid}
 VERSION=${project.version}
 
 java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n \
-     -classpath ${PROJECT_PATH}/conf;${PROJECT_PATH}/lib
+     -classpath ${PROJECT_PATH}/conf:${PROJECT_PATH}/lib \
      -jar ${PROJECT_PATH}/lib/${ARTIFACTID}-${VERSION}.jar --debug
--- service/service-foundation/src/main/bin/prod/start.sh
+++ service/service-foundation/src/main/bin/prod/start.sh
@@ -4,5 +4,5 @@ PROJECT_PATH=$(cd `dirname $0`/..; pwd)
 ARTIFACTID=${project.artifactid}
 VERSION=${project.version}
 
-java -classpath ${PROJECT_PATH}/conf;${PROJECT_PATH}/lib
+java -classpath ${PROJECT_PATH}/conf:${PROJECT_PATH}/lib \
      -jar ${PROJECT_PATH}/lib/${ARTIFACTID}-${VERSION}.jar
